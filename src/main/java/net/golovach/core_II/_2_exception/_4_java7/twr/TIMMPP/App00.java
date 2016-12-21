e159ef9b40429d354275e9c68add799ba5fd6da9
Andrey Litvinov <litwinoff@gmail.com>
2016-12-21 17:26:30 +0300

    try-with-resourses in action

--- src/main/java/net/golovach/core_II/_2_exception/_4_java7/twr/TIMMPP/App00.java
+++ src/main/java/net/golovach/core_II/_2_exception/_4_java7/twr/TIMMPP/App00.java
@@ -3,8 +3,8 @@ package net.golovach.core_II._2_exception._4_java7.twr.TIMMPP;
 public class App00 {
 
 	public static void main(String[] args) throws Exception {
-		try (X x0  = new X(); X x1  = new X(); X x2  = new X();) {
-			
+		try (X x0  = new X("0"); X x1  = new X("1"); X x2  = new X("2");) {
+			System.out.println("try");
 		}
 
 	}
@@ -12,10 +12,17 @@ public class App00 {
 }
 
 class X implements AutoCloseable {
+	
+	private String name;
+	
+	public X(String name) {		
+		this.name = name;
+		System.out.println("new:" + name);
+	}
 
 	@Override
 	public void close() throws Exception {
-				
+		System.out.println("close:" + name);				
 	}
 	
 }
